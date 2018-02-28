2ec2bcd4426e425f8cd1c7e5e7881df91ddfe66d
Joseph Verburg <josephverburg@gmail.com>
2018-02-28 17:55:37 +0100

    Start on model

--- Wireshark/model.py
+++ Wireshark/model.py
@@ -1,3 +1,61 @@
+def packetToInfo(packet):
+	radio = packet.layers[1]
+	wlan = packet.layers[2]
+
+	# Beacon
+	if int(wlan.fc_type_subtype) == 8:
+		wlan2 = packet.layers[3]
+		info = {
+			"source": "beacon",
+			"bandwith": 20,
+			"channel": radio.channel,
+			"ssid": wlan2.ssid
+		}
+		if hasattr(wlan2, 'ht_capabilities'):
+			info["n"] = True
+			if wlan2.ht_capabilities_width == 1:
+				info["bandwith"] = 40
+
+		if hasattr(wlan2, 'vht_capabilities'):
+			info["ac"] = True
+			widths = wlan2.vht_capabilities_supportedchanwidthset.int_value
+			if widths == 1:
+				info["bandwith"] = 40
+			elif widths == 2:
+				info["bandwith"] = 80
+			elif widths == 3:
+				info["bandwith"] = 160
+
+		print(info)
+		return info
+
+	if int(wlan.fc_type_subtype) == 5:
+		wlan2 = packet.layers[3]
+		info = {
+			"source": "probe response",
+			"bandwith": 20,
+			"channel": radio.channel,
+			"ssid": wlan2.ssid
+		}
+		if hasattr(wlan2, 'ht_capabilities'):
+			info["n"] = True
+			if wlan2.ht_capabilities_width == 1:
+				info["bandwith"] = 40
+
+		if hasattr(wlan2, 'vht_capabilities'):
+			info["ac"] = True
+			widths = wlan2.vht_capabilities_supportedchanwidthset.int_value
+			if widths == 1:
+				info["bandwith"] = 40
+			elif widths == 2:
+				info["bandwith"] = 80
+			elif widths == 3:
+				info["bandwith"] = 160
+		print(info)
+		return info
+
+	return None
+
 class packageFiltered:
 	def packagefiltered():
 		print ("Constructor called")
--- Wireshark/process_captures.py
+++ Wireshark/process_captures.py
@@ -1,10 +1,11 @@
 
 import pyshark
+import model
 
 for channel in range(1, 14):
 	file = './captures/channel_' + str(channel) + '.cap'
 	capture = pyshark.FileCapture(input_file=file)
 
 	for packet in capture:
+		info = model.packetToInfo(packet)
 
-		print(channel, packet)
