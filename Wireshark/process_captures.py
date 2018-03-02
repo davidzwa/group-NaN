1c43c84202b47f08eebb98d63d0a511e4bb4f11a
Joseph Verburg <josephverburg@gmail.com>
2018-03-02 16:51:17 +0100

    More data and data parsing

--- Wireshark/process_captures.py
+++ Wireshark/process_captures.py
@@ -1,11 +1,34 @@
 
 import pyshark
 import model
+import os
+from pprint import pprint
+import glob
+import json
 
-for channel in range(1, 14):
-	file = './captures/channel_' + str(channel) + '.cap'
+aps = {}
+channels = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for file in glob.glob("./captures/*.pcapng"):
+	print(file)
 	capture = pyshark.FileCapture(input_file=file)
 
 	for packet in capture:
 		info = model.packetToInfo(packet)
+		if info is None:
+			continue
+
+		if info["mac"] in aps:
+			continue
+
+		aps[info["mac"]] = info
+		channels[info["channel"]] += 1
+
+
+pprint(channels)
+
+data = json.dumps(aps, indent='\t')
+with open('aps.json', 'w') as file:
+	file.write(data)
+	file.close()
 
